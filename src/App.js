d4845e58843444114d24a964bba8acc1af3caaa0
kurohage <517326+kurohage@users.noreply.github.com>
2019-11-05 19:08:58 +0300

    Completed up to side bar.

--- src/App.js
+++ src/App.js
@@ -9,16 +9,40 @@ import AuthorsList from "./AuthorsList";
 import AuthorDetail from "./AuthorDetail";
 
 class App extends Component {
+  state = {
+    currentAuthor: null,
+    authors: authors
+  };
+
+  selectAuthor = author => {
+    this.setState({ currentAuthor: author });
+  };
+
+  ListOrDetail = () => {
+    if (!this.state.currentAuthor)
+      return (
+        <AuthorsList
+          authors={this.state.authors}
+          selectAuthor={this.selectAuthor}
+        />
+      );
+    else
+      return (
+        <AuthorDetail
+          author={this.state.currentAuthor}
+          key={this.state.currentAuthor.first_name}
+        />
+      );
+  };
+
   render() {
     return (
       <div id="app" className="container-fluid">
         <div className="row">
           <div className="col-2">
-            <Sidebar />
+            <Sidebar selectAuthor={this.selectAuthor} />
           </div>
-          <div className="content col-10">
-            <AuthorsList authors={authors} />
-          </div>
+          <div className="content col-10">{this.ListOrDetail()}</div>
         </div>
       </div>
     );
--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 
 // Components
 import AuthorCard from "./AuthorCard";
-import AuthorDetail from "./AuthorDetail";
+//import AuthorDetail from "./AuthorDetail";
 
-function AuthorsList(props) {
+class AuthorsList extends Component {
+  /*
+  // old code that printed unique details per card. This is not what the task is asking, though.
   const authorCards = props.authors.map(
     author => (
       <AuthorCard key={author.first_name + author.last_name} author={author} />,
@@ -16,13 +18,23 @@ function AuthorsList(props) {
       )
     )
   );
+  */
+  authorCards = this.props.authors.map(author => (
+    <AuthorCard
+      key={author.first_name + author.last_name}
+      author={author}
+      selectAuthor={this.props.selectAuthor}
+    />
+  ));
 
-  return (
-    <div className="authors">
-      <h3>Authors</h3>
-      <div className="row">{authorCards}</div>
-    </div>
-  );
+  render() {
+    return (
+      <div className="authors">
+        <h3>Authors</h3>
+        <div className="row">{this.authorCards}</div>
+      </div>
+    );
+  }
 }
 
 export default AuthorsList;
