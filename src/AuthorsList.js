61af7fbf590faa1fbf1c601948e3c5fd34a862ac
kurohage <517326+kurohage@users.noreply.github.com>
2019-11-05 19:40:28 +0300

    Almost functioning search bar.

--- src/AuthorsList.js
+++ src/AuthorsList.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 // Components
 import AuthorCard from "./AuthorCard";
+import SearchBar from "./SearchBar";
 //import AuthorDetail from "./AuthorDetail";
 
 class AuthorsList extends Component {
@@ -19,19 +20,36 @@ class AuthorsList extends Component {
     )
   );
   */
-  authorCards = this.props.authors.map(author => (
-    <AuthorCard
-      key={author.first_name + author.last_name}
-      author={author}
-      selectAuthor={this.props.selectAuthor}
-    />
-  ));
+
+  state = {
+    authors: this.props.authors
+  };
+
+  filterAuthors = query => {
+    const searched = this.state.authors.filter(author => {
+      return (author.first_name + author.last_name)
+        .toLowerCase()
+        .includes(query);
+    });
+
+    this.setState({ authors: searched });
+  };
 
   render() {
+    const authorCards = this.state.authors.map(author => (
+      <AuthorCard
+        author={author}
+        key={author.first_name + author.last_name}
+        selectAuthor={this.props.selectAuthor}
+      />
+    ));
+
     return (
       <div className="authors">
+        <SearchBar filterAuthors={this.filterAuthors} />
+        <br />
         <h3>Authors</h3>
-        <div className="row">{this.authorCards}</div>
+        <div className="row">{authorCards}</div>
       </div>
     );
   }
